19ab2f766495a304be99b263583b98f143a8d7b9
David O'Neill <doneill612@gmail.com>
2025-10-19 19:17:17 -0400

    feat: live-grep keybinding

--- lua/don/plugins/telescope.lua
+++ lua/don/plugins/telescope.lua
@@ -38,5 +38,7 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files by name" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
   end
 }
