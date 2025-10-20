0c9a92b1fdab288a48f60003a22b3d32dfdf687c
David O'Neill <doneill612@gmail.com>
2025-10-19 21:52:59 -0400

    feat: finish solarized osaka integration

--- lua/don/plugins/colorscheme.lua
+++ lua/don/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
 	"craftzdog/solarized-osaka.nvim",
-	lazy = true,
+	lazy = false,
 	priority = 1000,
 	opts = function()
 		return {
 			transparent = true,
 		}
+	end,
+	config = function(_, opts)
+		require("solarized-osaka").setup(opts)
+		vim.cmd([[colorscheme solarized-osaka]])
 	end,
 }
