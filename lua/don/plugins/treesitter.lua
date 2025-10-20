12a4c9bc1312eb51793b66df2f763b24cf5e5c47
David O'Neill <doneill612@gmail.com>
2025-10-19 21:34:48 -0400

    feat: update color scheme, diagnotic issues w/ treesitter resolved

--- lua/don/plugins/treesitter.lua
+++ lua/don/plugins/treesitter.lua
@@ -1,31 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({  -- Note: also "configs" not "config"
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "c",
-        "query",
-        "typescript",
-        "javascript",
-        "python",
-        "go",
-        "c_sharp"
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false
-      },
-      indent = {
-        enable = true,
-        disable = { "python", "c" }
-      }
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"c",
+				"query",
+				"typescript",
+				"javascript",
+				"python",
+				"go",
+				"c_sharp",
+			},
+			sync_install = false,
+			ignore_install = {},
+			modules = {},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+				disable = { "python", "c" },
+			},
+		})
+	end,
 }
-
