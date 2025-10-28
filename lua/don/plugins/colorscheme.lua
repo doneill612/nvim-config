6659580cc7790e4cf3dd4b3a283d15e8936e84cc
David O'Neill <doneill612@gmail.com>
2025-10-28 16:43:36 -0500

    feat: colorscheme selector

--- lua/don/plugins/colorscheme.lua
+++ lua/don/plugins/colorscheme.lua
@@ -1,14 +1,40 @@
-return {
-	"craftzdog/solarized-osaka.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = function()
+local colorSchemes = {
+	solarized_osaka = function()
 		return {
-			transparent = false,
+			"craftzdog/solarized-osaka.nvim",
+			lazy = false,
+			priority = 1000,
+			opts = function()
+				return {
+					transparent = false,
+				}
+			end,
+			config = function(_, opts)
+				require("solarized-osaka").setup(opts)
+				vim.cmd([[colorscheme solarized-osaka]])
+			end,
 		}
 	end,
-	config = function(_, opts)
-		require("solarized-osaka").setup(opts)
-		vim.cmd([[colorscheme solarized-osaka]])
+	kanagawa = function(themeName)
+		return {
+			"rebelot/kanagawa.nvim",
+			lazy = false,
+			priority = 1000,
+			config = function()
+				local themeColor = require("kanagawa")
+				themeColor.setup({
+					compile = false,
+					undercurl = true,
+					commentStyle = { italic = true },
+					keywordStyle = { italic = true, bold = true },
+					transparent = false,
+					theme = themeName,
+				})
+				vim.cmd("colorscheme kanagawa")
+				themeColor.load(themeName)
+			end,
+		}
 	end,
 }
+
+return colorSchemes.kanagawa("lotus")
